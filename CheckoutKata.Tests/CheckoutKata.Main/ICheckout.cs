f77186f8bce01c81a18d1bda3dfe18fba96606e5
Chris Pepper <chrispepper1989@gmail.com>
2025-02-13 17:51:49 +0000

    chore: wrong unit library (I prefer xunit)

--- CheckoutKata.Tests/CheckoutKata.Main/ICheckout.cs
+++ CheckoutKata.Tests/CheckoutKata.Main/ICheckout.cs
@@ -5,3 +5,16 @@ public interface ICheckout
     void Scan(string item);
     int GetTotalPrice();
 }
+
+public class Checkout : ICheckout
+{
+    public void Scan(string item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetTotalPrice()
+    {
+        throw new NotImplementedException();
+    }
+}
--- CheckoutKata.Tests/CheckoutKata.Tests/CheckoutTests.cs
+++ CheckoutKata.Tests/CheckoutKata.Tests/CheckoutTests.cs
@@ -1,34 +1,20 @@
-﻿namespace CheckoutKata.Tests;
+﻿using CheckoutKata.Main;
 
-/*
- * Checkout Kata
-Implement the code for a checkout system that handles pricing schemes such as “pineapples
-cost 50, three pineapples cost 130”.
-Implement the code for a supermarket checkout that calculates the total price of a number of
-items. In a normal supermarket, things are identified using Stock Keeping Units, or SKUs. In our
-store, we’ll use individual letters of the alphabet (A, B, C, and so on). Our goods are priced
-individually. In addition, some times are multi-priced: buy n of them and they’ll cost you y
-pence. For example, item A might cost 50 individually , but this week we have a special offer:
-buy three As and they’ll cost you 130. In fact the prices are:
-SKU Unit Price Special Price
-A 50 3 for 130
-B 30 2 for 45
-C 20
-D 15
-The checkout accepts items in any order, so that if we scan a B, an A, and another B, we’ll
-recognise the two Bs and price them at 45 (for a total price so far of 95). The pricing changes
-frequently, so pricing should be independent to the c
- */
+namespace CheckoutKata.Tests;
+
+
 public class CheckoutTests
 {
+    
     [SetUp]
     public void Setup()
     {
     }
 
-    [Test]
+    [Theory]
+    [inlin]
     public void Test1()
     {
-        Assert.Pass();
+        
     }
 }
