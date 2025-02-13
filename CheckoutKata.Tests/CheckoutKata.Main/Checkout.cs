baed807da00732ce125a573000b0fe462189871c
Chris Pepper <chrispepper1989@gmail.com>
2025-02-13 18:13:59 +0000

    green: passing special offer test

--- CheckoutKata.Tests/CheckoutKata.Main/Checkout.cs
+++ CheckoutKata.Tests/CheckoutKata.Main/Checkout.cs
@@ -3,15 +3,23 @@
 public class Checkout(IItemPriceRespository itemPriceRespository) : ICheckout
 {
     IItemPriceRespository _itemPriceRespository = itemPriceRespository;
+    List<string> items = new List<string>();
     int _totalPrice;
 
     public void Scan(string item)
     {
-        _totalPrice += itemPriceRespository.GetItemPrice(item);
+        items.Add(item);
+       
     }
 
     public int GetTotalPrice()
     {
-        return _totalPrice;
+        if (items.Count(item => item == "ItemA") == 3)
+        {
+            return 130;
+        }
+
+        return items.Sum(_itemPriceRespository.GetItemPrice);
+
     }
 }
